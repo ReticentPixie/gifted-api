0902829286b71f21ce352529cfd0cfd1f55b262f
Diana Barney <diana.aiken@gmail.com>
2021-10-28 14:17:13 -0400

    added transaction.js controller file with index and create route

---
+++ controllers/transactions.js
@@ -0,0 +1,34 @@
+// =======================================
+//              DEPENDENCIES
+// =======================================
+const express = require('express')
+const Transactions = require('../models/transaction')      // require relevant model
+const router = express.Router();                           // create router object
+
+// =======================================
+//              ROUTES
+//          Follow INDUCES
+// =======================================
+// ----- INDEX Route -----
+router.get('/', async (req, res) => {
+    try {
+        res.json(await Transactions.find({}));
+    } catch (error) {
+        res.status(401).json({message: 'Login required to continue'})
+    }
+})
+
+// ----- Create Route -----
+router.post('/', async (req, res) => {
+    try {
+        res.json(await Transactions.create(req.body))
+    } catch (error) {
+        res.status(401).json({message: 'Login required to continue'})
+    }
+})
+
+
+// =======================================
+//         EXPORT ROUTER OBJECT
+// =======================================
+module.export = router
