8ab98f2a73c64bad55b37d3fee6eda220991955c
Diana Barney <diana.aiken@gmail.com>
2021-10-29 12:21:56 -0400

    added notes controller and schema

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,7 +1,8 @@
 // =======================================
 //              DEPENDENCIES
 // =======================================
-const express = require('express')
+const express = require('express');
+const transaction = require('../models/transaction');
 const Transactions = require('../models/transaction')       // require relevant model
 const router = express.Router();                            // create router object
 
@@ -27,6 +28,19 @@ router.post('/', async (req, res) => {
         res.json(await Transactions.create(req.body))
     } catch (error) {
         res.status(401).json({message: 'Login required to continue'})
+    }
+})
+
+// EXAMPLE of how to do an embedded schema
+// this is a nested resource route
+router.post('/:id/notes', async (req, res) => {
+    try {
+       const transaction = await Transctions.findById(req.params.id);
+       transaction.notes.push(req.body);            // pushes the data into the notes array in memory only
+       await transaction.save();                    // saves the data to persist the changes in mongoDB
+       res.json(transaction);
+    } catch (error) {
+        res.status(401).json({message: 'Something went wrong.'})
     }
 })
 
--- models/transaction.js
+++ models/transaction.js
@@ -11,12 +11,21 @@ const Schema = mongoose.Schema
 // =======================================
 //              DEFINE SCHEMA
 // =======================================
+// the noteSchema is an example of an embedded schema
+
+const noteSchema = new Schema ({
+    content: String,
+    createdBy: String,              // our logged in user
+}, { timestamps: true });
+
+
 const transactionSchema = new Schema ({
     eventId: {type: String, required: true},
     giftId: {type: String, required: true},
     recipientId: {type: String, required: true},
     date: Date,
     managedBy: String,              // this is the uid from google firebase for authentication purposes
+    todos: [noteSchema]
 }, { timestamps: true }
 )
 
